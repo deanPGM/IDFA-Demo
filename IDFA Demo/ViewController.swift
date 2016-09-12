ea1e3217c31a10c537e475336303bd706d67cd56
Deano <dean@murphyapps.co>
2016-09-12 14:44:20 +0100

    Initial commit.

--- IDFA Demo/ViewController.swift
+++ IDFA Demo/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import AdSupport
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var idfaLabel: UILabel!
+    @IBOutlet weak var LimitAdTrackingLabel: UILabel!
 
+    @IBAction func refreshButton(_ sender: AnyObject) {
+        
+        getIDFA()
+    }
+    
+    func getIDFA() {
+        
+        let advertisingIdManager = ASIdentifierManager()
+        let IDFA = advertisingIdManager.advertisingIdentifier
+        let isTracking = advertisingIdManager.isAdvertisingTrackingEnabled
+        
+        switch isTracking {
+        case false:
+            
+            LimitAdTrackingLabel.text = "Limit Ad Tracking: Enabled"
+        default:
+            
+            LimitAdTrackingLabel.text = "Limit Ad Tracking: Disabled"
+        }
+        
+        idfaLabel.text = "IDFA: \(IDFA!)"
+        
+        print(IDFA, isTracking)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        getIDFA()
+        
     }
 
     override func didReceiveMemoryWarning() {
